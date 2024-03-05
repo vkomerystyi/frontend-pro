7bdc0f099d0457536adf03d680f5c8b113bb7e7a
Vadym <v.komerystyi@gmail.com>
2024-03-05 22:54:29 +0200

    start home task 16

---
+++ lesson-12-Events-part 2/data.js
@@ -0,0 +1,47 @@
+const data = [
+  {
+    name: 'Овочі',
+    product: [
+      {
+        name: 'Помідор',
+        description: 'Опис про Помідорі',
+        prise: 70,
+      },
+      {
+        name: 'Огірок',
+        description: 'Опис про Огірка',
+        prise: 70,
+      },
+    ],
+  },
+  {
+    name: 'Фрукти',
+    product: [
+      {
+        name: 'Яблоко',
+        description: 'Опис про Яблоке',
+        prise: 70,
+      },
+      {
+        name: 'Груша',
+        description: 'Описание о Груше',
+        prise: 70,
+      },
+    ],
+  },
+  {
+    name: 'Сладкое',
+    product: [
+      {
+        name: 'Зефир',
+        description: 'Описание о Зефире',
+        prise: 70,
+      },
+      {
+        name: 'Мармелад',
+        description: 'Описание о Мармеладе',
+        prise: 70,
+      },
+    ],
+  },
+]
--- lesson-12-Events-part 2/script.js
+++ lesson-12-Events-part 2/script.js
@@ -8,3 +8,37 @@
 Клік на товар – інформацію про товар у правому блоці.
 В інформації товару - кнопка "купити"
 При натисканні на “купити” з'являється повідомлення, що товар куплено та повернення у вихідний стан програми (коли відображається лише список категорій) */
+function showCategory() {
+  const categories = document.querySelector('.category')
+
+  for (let i = 0; i < data.length; i++) {
+    const elem = document.createElement('div')
+    elem.textContent = data[i].name
+    elem.setAttribute('data-category', i)
+    elem.addEventListener('click', showProducts)
+    categories.appendChild(elem)
+  }
+}
+
+function showProducts(event) {
+  const categoryIndex = event.target.getAttribute('data-category')
+  const products = data[categoryIndex].product
+  const product = document.querySelector('.product')
+  product.innerHTML = ''
+
+  for (let i = 0; i < products.length; i++) {
+    const elem = document.createElement('div')
+    elem.textContent = products[i].name
+    elem.setAttribute('data-category', categoryIndex)
+    elem.setAttribute('data-product', i)
+    elem.addEventListener('click', showDetails)
+    product.appendChild(elem)
+  }
+}
+function showDetails(event) {
+  const categoryIndex = event.target.getAttribute('data-category')
+  const productIndex = event.target.getAttribute('data-product')
+  const description = data[categoryIndex].product[productIndex].description
+  console.log(description)
+}
+showCategory()
