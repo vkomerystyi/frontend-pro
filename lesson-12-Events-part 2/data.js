0485d2fbe80229516f838a2a5baf65700746de12
Vadym <v.komerystyi@gmail.com>
2024-03-06 18:06:35 +0200

    task 14

---
+++ lesson-12-Events-part 2/data.js
@@ -0,0 +1,47 @@
+const data = [
+  {
+    name: 'Овочі',
+    product: [
+      {
+        name: 'Помідор',
+        description: 'Опис про Помідорі',
+        price: 65,
+      },
+      {
+        name: 'Огірок',
+        description: 'Опис про Огірка',
+        price: 36,
+      },
+    ],
+  },
+  {
+    name: 'Фрукти',
+    product: [
+      {
+        name: 'Яблоко',
+        description: 'Опис про Яблоке',
+        price: 15,
+      },
+      {
+        name: 'Груша',
+        description: 'Описание о Груше',
+        price: 10,
+      },
+    ],
+  },
+  {
+    name: 'Сладкое',
+    product: [
+      {
+        name: 'Зефир',
+        description: 'Описание о Зефире',
+        price: 70,
+      },
+      {
+        name: 'Мармелад',
+        description: 'Описание о Мармеладе',
+        price: 80,
+      },
+    ],
+  },
+]
